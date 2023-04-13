a499342e80860ef7cccb883a39c3183319265596
IhorHodebskyi <121040851+IhorHodebskyi@users.noreply.github.com>
2023-04-13 21:16:31 +0300

    task-02

--- js/task-02.js
+++ js/task-02.js
@@ -7,4 +7,14 @@ const ingredients = [
   "Condiments",
 ];
 
-document.createElement("li");
+const elem = ingredients.map((option) => {
+  const itemEl = document.createElement("li");
+  itemEl.classList.add("item");
+  itemEl.textContent = option;
+  console.log(itemEl);
+  return itemEl;
+});
+
+const listEl = document.querySelector("ul#ingredients");
+
+listEl.append(...elem);
